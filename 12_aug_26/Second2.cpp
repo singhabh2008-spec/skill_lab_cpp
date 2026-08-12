f530369b1df4050612f509fb3be2943d2c93e5bb
singhabh2008-spec <singhabh2008@gmail.com>
2026-08-12 10:41:36 +0530

    added remaining files

---
+++ 12_aug_26/Second2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    double a;
+    cout << "enter the valuse of a";
+    cin >> a;
+    cout << "value of a is:" << a << endl;
+    double b;
+    cout << "enter the valuse of b";
+    cin >>b;
+    cout << "value of b is:" << b << endl;
+    cout << "sum of a ans b is " << a + b;
+    return 0;
+}
